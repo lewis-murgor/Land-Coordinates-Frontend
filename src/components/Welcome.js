d4b2bddb4ddfdea5af685276222ae557d513cc8d
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-09 16:40:24 +0300

    create welcome function

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,3 +1,24 @@
-function Welcome () {};
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Welcome () {
+    return (
+        <div>
+            <header>
+                <h1>Welcome to the Land Coordinates website</h1>
+            </header>
+            <main>
+                <p>Find and manage lands specific coordinates.</p>
+                <br/>
+                <Link to='/login'>
+                    <button>Sign in</button>
+                </Link>
+                <Link to='/signup'>
+                    <button>Create account</button>
+                </Link>
+            </main>
+        </div>
+    );
+};
 
 export default Welcome;
