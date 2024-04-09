4f95d0838b8b8951330c44b00f74c4dcb93201c1
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-09 11:33:13 +0300

    render signup component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
+import SignUp from './auth/SignUp';
 
 function App() {
   return (
     <div>
       <h1>Land Map</h1>
+      <SignUp />
     </div>
   );
 }
