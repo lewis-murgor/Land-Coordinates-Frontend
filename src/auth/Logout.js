41d9dd03e970737369f02cf5ab7f4bbe50e6b579
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-09 15:20:34 +0300

    add the useNavigate hook

--- src/auth/Logout.js
+++ src/auth/Logout.js
@@ -1,34 +1,33 @@
 import React, { useEffect } from 'react';
-//import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { ENDPOINTS } from '../api';
 
 function Logout () {
-    //const history = useHistory();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        logoutUser();
-    }, []);
-
-    const logoutUser = async () => {
-        try {
-            const response = await fetch(ENDPOINTS.LOGOUT, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+        const logoutUser = async () => {
+            try {
+                const response = await fetch(ENDPOINTS.LOGOUT, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                });
+    
+                if (response.ok) {
+                    localStorage.removeItem('token');
+                    navigate('/login')
+                } else {
+                    throw new Error('Logout failed');
                 }
-            });
-
-            if (response.ok) {
-                localStorage.removeItem('token');
-                //history.push('/login')
-            } else {
-                throw new Error('Logout failed');
+            } catch (error) {
+                console.error('Logout error:', error);
             }
-        } catch (error) {
-            console.error('Logout error:', error);
-        }
-    };
+        };
+        logoutUser();
+    }, [navigate]);
 
     return (
         <div>
