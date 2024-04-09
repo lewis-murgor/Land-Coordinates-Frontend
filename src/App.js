153f32595adeacc19824a7b3e96e570c024f588c
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-09 15:15:38 +0300

    define routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 //import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
 import './App.css';
 import Login from './auth/Login';
+import SignUp from './auth/SignUp';
+import Landmap from './components/Landmap';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div>
-      <h1>Land Map</h1>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={Welcome}/>
+        <Route exact path='/landmap' Component={Landmap}/>
+        <Route exact path='/login' Component={Login}/>
+        <Route exact path='/signup' Component={SignUp}/>
+      </Routes>
+    </Router>
   );
 }
 
