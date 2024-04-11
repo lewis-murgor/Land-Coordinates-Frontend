17053a7179c3fe9bc58c85df6d1f38312cd553be
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-11 14:33:11 +0300

    add lands endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://127.0.0.1:8000/';
+const BASE_URL = 'http://127.0.0.1:8000';
 
 export const ENDPOINTS = {
     LOGIN: `${BASE_URL}/auth/token/login`,
     LOGOUT: `${BASE_URL}/auth/token/logout`,
     SIGNUP: `${BASE_URL}/auth/users/`,
-    // LANDS: `${BASE_URL}/lands`,
+    LANDS: `${BASE_URL}/lands`,
   };
