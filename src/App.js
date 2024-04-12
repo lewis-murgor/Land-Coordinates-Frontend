cfbc19ac083350792621fe508041ff04093c44f1
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-12 14:08:11 +0300

    add more routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Login from './auth/Login';
 import SignUp from './auth/SignUp';
 import Landmap from './components/Landmap';
 import Welcome from './components/Welcome';
+import Lands from './components/Lands';
+import AddLand from './components/AddLand';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route exact path='/landmap' Component={Landmap}/>
         <Route exact path='/login' Component={Login}/>
         <Route exact path='/signup' Component={SignUp}/>
+        <Route exact path='/lands' Component={Lands}/>
+        <Route exact path='/addland' Component={AddLand}/>
       </Routes>
     </Router>
   );
