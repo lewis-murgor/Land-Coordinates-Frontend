e065ff6c11081681d34757e32f729835dcb39693
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-16 15:17:12 +0300

    import bootstrap and load navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './auth/Login';
 import SignUp from './auth/SignUp';
 import Landmap from './components/Landmap';
 import Welcome from './components/Welcome';
 import Lands from './components/Lands';
 import AddLand from './components/AddLand';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      < Navbar />
       <Routes>
         <Route exact path='/' Component={Welcome}/>
         <Route exact path='/map/:id' Component={Landmap}/>
