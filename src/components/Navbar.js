286f7b2173eaafa563d9e5dfe7dfda754af75100
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-17 16:10:54 +0300

    pass setIsAuthenticated function to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,31 +3,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logout from '../auth/Logout';
 
-function Navbar ({isAuthenticated}) {
+function Navbar ({ isAuthenticated, setIsAuthenticated }) {
+
     return (
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
             <div className='container'>
-                <div className='navbar-header header'>
-                    <a className='navbar-brand' href='/lands'>Land Coordinates</a>
-                </div>
+                <Link className='navbar-brand' href='/lands'>Land Coordinates</Link>
                 <div className='collapse navbar-collapse'>
-                    <ul className='nav navbar-nav navbar-right'>
-                    {isAuthenticated ? (
-                        <>
-                            <li className='nav-item'>
-                                < Logout />
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className='nav-item'>
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className="nav-link" to="/signup">Sign Up</Link>
-                            </li>
-                        </>
-                    )}
+                    <ul className='navbar-nav custom-navbar'>
+                        { isAuthenticated ? (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/addland">Add Land</Link>
+                                </li>
+                                <li className='nav-item'>
+                                    <Logout setIsAuthenticated={setIsAuthenticated} />
+                                </li>
+                            </>
+                    
+                        ) : (
+                            <>
+                                <li className='nav-item'>
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                                <li className='nav-item'>
+                                    <Link className="nav-link " to="/signup">Sign Up</Link>
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </div>
