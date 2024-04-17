add95c511453c03cc298df7cf7cecf75ac0fe8d7
Lewis <lewis.murgor@student.moringaschool.com>
2024-04-17 17:30:26 +0300

    add bootstrap classes and style the page

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ENDPOINTS } from '../api';
+import './auth.css';
 
 function Login ({ setIsAuthenticated }) {
     const [username, setUsername] = useState('');
@@ -37,24 +38,37 @@ function Login ({ setIsAuthenticated }) {
 
     return (
         <div className='container'>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text"
-                    placeholder="Username"
-                    className="form-control"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
+            <div className='row'>
+                <div className='col-md-4'></div>
+                <div className='col-md-4'>
+                    <div className='card auth'>
+                        <div className='card-header'><h2>Login</h2></div>
+                        <div className='card-body'>
+                            <form onSubmit={handleSubmit}>
+                                <input 
+                                    type="text"
+                                    placeholder="Username"
+                                    className="form-control"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                                <br />
+                                <input 
+                                    type="password"
+                                    placeholder="Password"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <br /><br />
+                                <button className='btn btn-primary btn-md auth-button' type="submit">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-4'></div>
+            </div>
+            
         </div>
     );
 };
